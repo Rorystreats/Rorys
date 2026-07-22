9990317fecb84ab37ea6a79932de37cbfab69ed1
rorystreats <rorysfoods@gmail.com>
2026-07-22 14:39:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,44 @@
  */
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { AnimatePresence } from 'motion/react';
 import CartDrawer from './components/CartDrawer';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import ProductDetail from './pages/ProductDetail';
+import ScrollToTop from './components/ScrollToTop';
+import WhatsAppWidget from './components/WhatsAppWidget';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      {/* @ts-expect-error - React router types don't officially support key here but React does */}
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu/:categoryId" element={<Menu />} />
+        <Route path="/product/:slug" element={<ProductDetail />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 export default function App() {
   return (
     <CartProvider>
       <Router>
-        <div className="min-h-screen bg-[#FDFBF7] text-[#2C1818] font-sans selection:bg-[#6B1111] selection:text-[#FDFBF7]">
+        <ScrollToTop />
+        <div className="min-h-screen bg-[#FDFBF7] text-[#2C1818] font-sans selection:bg-[#6B1111] selection:text-[#FDFBF7] relative">
           <Navbar />
           <CartDrawer />
+          <WhatsAppWidget />
           
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/menu/:categoryId" element={<Menu />} />
-            <Route path="/product/:slug" element={<ProductDetail />} />
-          </Routes>
+          <AnimatedRoutes />
           
           <Footer />
         </div>
